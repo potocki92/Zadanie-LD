632ce98ff03df205e9deccf8c33d03e2d97dc5fd
potocki92 <mateusz.potocki92@gmail.com>
2023-10-26 12:11:38 +0200

    aktualizacja/ContactList
    Aktualizacja komponentu Contact:
    - Dodano importy komponentów `StyledLi`, `ContactButton` i `ContactList`.
    - Zmieniono zawartość komponentu, dodając listę elementów `<StyledLi>` i przycisków.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,22 @@
+import { StyledLi } from "../../global.styled";
+import { ContactButton, ContactList } from "./Contact.styled";
+
 export const Contact = () => {
-  return <></>;
+  return <ContactList>
+    <StyledLi fontWeight="500" fontSize="14px" lineHeight="24px" letterSpacing="0.06em">
+      <a>
+        +48 58 506 65 65
+      </a>
+    </StyledLi>
+    <StyledLi>
+      <ContactButton>
+        Zapytaj o ofertę
+      </ContactButton>
+    </StyledLi>
+    <StyledLi>
+      <ContactButton>
+        Umów jazdę próbną
+      </ContactButton>
+    </StyledLi>
+  </ContactList>;
 };
