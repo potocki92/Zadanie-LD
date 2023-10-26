e6c0e8280d05b6952b704d557fc0516dbe617415
potocki92 <mateusz.potocki92@gmail.com>
2023-10-26 12:13:40 +0200

    aktualizacja/Header.styled
    Aktualizacja komponentu Header:
    - Usunięto jedno zduplikowane `StyledHeader`, aby uniknąć konfliktu w definicji.
    - Ujednolicono styl komponentu `StyledHeader` w jednej definicji.
    - Dodano styl komponentu `HeaderWrapper`

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import { Container } from "../../global.styled";
 
-export const StyledHeader = styled(Container)`
-    background-color: #F5F5F5;
-    height: 84px;
-    display: flex;
-    align-items: center;
-    ${Container}
-`
+export const StyledHeader = styled.header`
+  background-color: #f5f5f5;
+  height: 84px;
+  display: flex;
+  align-items: center;
+`;
+
+export const HeaderWrapper = styled(Container)`
+  display: flex;
+  gap: 96px;
+`;
