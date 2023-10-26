32aa2642ef001ca37b700287dd4d18ae401f31fe
potocki92 <mateusz.potocki92@gmail.com>
2023-10-26 12:16:40 +0200

    dodano/Section.styled
    Dodano definicje stylów dla komponentu Section:
    - Utworzono komponent `StyledSection` do stylizacji sekcji.
    - Utworzono komponent `SectionWrapper` do stylizacji kontenera sekcji.
    - Użyto CSS Grid w `SectionWrapper` do konfiguracji układu siatki.
    - Zdefiniowano kształt komórek siatki za pomocą pseudoelementów `:nth-child`.
    - Użyto komponentów `SectionSpan` i `SectionText` do stylizacji tekstu w sekcji.
    - Określono rozmiar czcionki, wagę czcionki i kolor tekstu w sekcji.

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { Container } from "../../global.styled";
+
+export const StyledSection = styled.section`
+  padding: 104px 0;
+`;
+
+export const SectionWrapper = styled(Container)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(7, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+
+  :nth-child(1) {
+    grid-area: 1/1/4/4;
+  }
+  :nth-child(2) {
+    grid-area: 2/1/6/1;
+  }
+  :nth-child(3) {
+    grid-area: 7/1/7/1;
+  }
+  :nth-child(4) {
+    grid-area: 2/2/8/2;
+    margin-top: 128px;
+    justify-self: end;
+  }
+
+  > :nth-child(2) {
+    margin-top: 68px;
+  }
+`;
+
+export const SectionSpan = styled.span`
+  font-size: ${(props) => props.fontSize || "60px"};
+  font-weight: 300;
+`;
+
+export const SectionText = styled.p`
+  font-weight: 400;
+  font-size: 20px;
+  color: #ffffff80;
+`;
