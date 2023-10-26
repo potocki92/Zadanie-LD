342bd21f78261a49b85807fee4268298f3d97882
potocki92 <mateusz.potocki92@gmail.com>
2023-10-26 12:19:04 +0200

    aktualizacja/Home
    Aktualizacja komponentu Home:
    - Poprawiono import komponentu `Header` przez usunięcie zbędnego dublowania importu.
    - Dodano import komponentów `Hero` i `Section`.
    - Zaktualizowano zawartość komponentu `Home` przez umieszczenie komponentów `Header`, `Hero`, i `Section` w odpowiednich blokach `return`.
    - Usunięto nadmiarowy element `<Header />` poza blokiem `return`.
    
    Ten opis komitu odzwierciedla poprawę importów oraz organizację zawartości komponentu `Home`.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import Header from "../components/Header/Header"
+import Header from "../components/Header/Header";
+import { Hero } from "../components/Hero/Hero";
+import { Section } from "../components/Section/Section";
 
 const Home = () => {
-    return (
-        <>
-        <Header />
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Section />
+    </>
+  );
+};
 
-export default Home
+export default Home;
