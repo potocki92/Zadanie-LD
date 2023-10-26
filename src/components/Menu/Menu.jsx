f2489315b30499c6a53d766502ac67ee54d62a41
potocki92 <mateusz.potocki92@gmail.com>
2023-10-26 12:15:01 +0200

    aktualizacja/Menu
    Aktualizacja komponentu Menu:
    - Usunięto nadmiarowy pusty element `<></>`.
    - Dodano element `<MenuList>`, który zawiera listę elementów `<StyledLi>`.
    - Każdy element `<StyledLi>` zawiera tekst i odpowiednie style takie jak `lineHeight` i `fontWeight`.

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,21 @@
+import { StyledLi } from "../../global.styled";
+import { MenuList } from "./Menu.styled";
+
 export const Menu = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <MenuList>
+      <StyledLi lineHeight="26px" fontWeight="300">
+        <span>Samochody</span>
+      </StyledLi>
+      <StyledLi lineHeight="26px" fontWeight="300">
+        <span>Jazda próbna</span>
+      </StyledLi>
+      <StyledLi lineHeight="26px" fontWeight="300">
+        <span>Oferta</span>
+      </StyledLi>
+      <StyledLi lineHeight="26px" fontWeight="300">
+        <span>Salony Cupra</span>
+      </StyledLi>
+    </MenuList>
+  );
+};
