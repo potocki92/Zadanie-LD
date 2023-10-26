036649c24025c367a35d878463fb3fe789bf3fbf
potocki92 <mateusz.potocki92@gmail.com>
2023-10-26 12:15:35 +0200

    aktualizacja/Nav
    @@ -1,9 +1,12 @@
    import { Contact } from "../Contact/Contact";
    import { Menu } from "../Menu/Menu";
    import { StyledNav } from "./Nav.styled";
    
    export const Nav = () => {
      return (
        <StyledNav>
    
          <Menu/>
          <Contact />
        </StyledNav>
      );
    };

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,12 @@
+import { Contact } from "../Contact/Contact";
+import { Menu } from "../Menu/Menu";
 import { StyledNav } from "./Nav.styled";
 
 export const Nav = () => {
   return (
     <StyledNav>
-      
+      <Menu/>
+      <Contact />
     </StyledNav>
   );
 };
