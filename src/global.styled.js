ea8715aa79008fc007df6e51d4719d96fe6c6a13
potocki92 <mateusz.potocki92@gmail.com>
2023-10-26 12:18:33 +0200

    aktualizacja/global.styled
    Aktualizacja definicji stylów globalnych i komponentów wspólnych:
    - Dodano atrybuty `width`, `margin`, oraz `@media` w `Container` do zarządzania szerokością kontenera na różnych rozdzielczościach.
    - Usunięto atrybuty `font-family` i `padding` w `body`.
    - Dodano nowe atrybuty stylizacji, takie jak `font-weight`, `font-size`, `line-height`, i `letter-spacing` w komponencie `StyledLi`.

--- src/global.styled.js
+++ src/global.styled.js
@@ -4,7 +4,7 @@ import { modernNormalize } from "styled-modern-normalize";
 export const ModernNormalize = createGlobalStyle`
 ${modernNormalize}
     body {
-        font-family: 'Cupra', sans-serif;
+        background-color: #1B1B1B
     }
     li, ul {
         list-style: none;
@@ -15,5 +15,20 @@ ${modernNormalize}
 `;
 
 export const Container = styled.div`
-  padding: 0 274px;
+  width: 1372px;
+  margin: 0 auto;
+  @media (max-width: 1430px) {
+    margin: 0 20px;
+  }
+`;
+
+export const StyledLi = styled.li`
+  a,
+  span,
+  button {
+    font-weight: ${(props) => props.fontWeight || "normal"};
+    font-size: ${(props) => props.fontSize || "16px"};
+    line-height: ${(props) => props.lineHeight || "48px"};
+    letter-spacing: ${(props) => props.letterSpacing || "normal"};
+  }
 `;
