3a4a068236f6cdd14e57748501bec24650a08d44
potocki92 <mateusz.potocki92@gmail.com>
2023-10-27 13:09:24 +0200

    aktualizacja/Home
    Zmieniono dane komponentu `Section`:
    - Teraz komponent `Section` przyjmuje tablicę danych `componentData`, która zawiera konfigurację dla dwóch sekcji.
    - Każdy obiekt w tablicy `componentData` zawiera różne style, tekst i obrazy dla sekcji.
    - Pierwsza sekcja ma kolor biały, maksymalną szerokość 553px, tytuł "WYPRÓBUJ", tekst opisujący samochody CUPRA oraz obraz modelu CUPRA Formentor.
    - Druga sekcja ma kolor biały, maksymalną szerokość 553px, tytuł "TYLKO DLA ODWAŻNYCH" o rozmiarze 40px, tekst opisujący model CUPRA oraz obraz modelu CUPRA Formentor z innym ujęciem.
    - W drugiej sekcji wyrenderowano również przycisk "Umów jazdę próbną" o określonych stylach.
    
    Zmieniono komponent `Home`:
    - W komponencie `Home` wyrenderowano komponenty `Header`, `Hero`, `Section` z przekazanymi danymi `componentData` oraz `Carousel`.
    
    Te zmiany wprowadzają bardziej elastyczną konfigurację sekcji na stronie głównej projektu, pozwalając na łatwe dostosowanie treści, stylów i obrazów w tych sekcjach.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,65 @@
+import { Carousel } from "../components/Carousel/Carousel";
+import { sectionImages } from "../components/Carousel/images";
 import Header from "../components/Header/Header";
 import { Hero } from "../components/Hero/Hero";
 import { Section } from "../components/Section/Section";
+
+const componentData = [
+  {
+    style: {
+      color: "white",
+      maxWidth: "553px",
+    },
+    span: {
+      sectionSpan: "WYPRÓBUJ",
+    },
+    text: {
+      sectionText:
+        "Zobacz, poczuj i przekonaj się, czym jest CUPRA. Poznaj samochody, które są wyjątkowym połączeniem technologii, mocy, elegancji i komfortu.",
+    },
+    image: {
+      imageSource: sectionImages.normal.cupra_formentor_front,
+      retina: sectionImages.retina.cupra_formentor_front_retina,
+      width: "787px",
+    },
+    button: {
+      shouldRenderButton: false,
+    },
+  },
+  {
+    style: {
+      color: "white",
+      maxWidth: "553px",
+    },
+    span: {
+      sectionSpan: "TYLKO DLA ODWAŻNYCH",
+      fontSize: "40px",
+    },
+    text: {
+      sectionText:
+        "Model CUPRA, który idealnie wpisuje się w DNA marki. Unikalny SUV coupé, wyjątkowy, odważny i innowacyjny – zapewni Ci to, co najlepsze w marce CUPRA.",
+    },
+    image: {
+      imageSource: sectionImages.normal.cupra_formentor_side,
+      retina: sectionImages.retina.cupra_formentor_side_retina,
+      width: "553px",
+    },
+    button: {
+      shouldRenderButton: true,
+      padding: "0 24px",
+      height: "48px",
+      text: "Umów jazdę próbną",
+    },
+  },
+];
 
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
-      <Section />
+      <Section data={componentData} />
+      <Carousel />
     </>
   );
 };
