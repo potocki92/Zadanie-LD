b35ea18b04645fd208a7ad02a2882bfa6bddb63c
potocki92 <mateusz.potocki92@gmail.com>
2023-10-27 13:05:49 +0200

    dodanie/images.js
    Dodano importowanie obrazów do projektu:
    - Zaimportowano obrazy karuzeli w różnych wariantach (normal i retina) dla różnych modeli samochodów.
    - Zaimportowano obrazy do sekcji pierwszej (Section one) w różnych wariantach (normal i retina) dla modelu Cupra Formentor.
    - Zdefiniowano obiekty `images` i `sectionImages` zawierające odnośniki do importowanych obrazów w dwóch wariantach (normal i retina).

---
+++ src/components/Carousel/images.js
@@ -0,0 +1,53 @@
+/** Carousel image */
+import cupra_ateca from "/image/carousel/cupra_ateca.png";
+import cupra_born from "/image/carousel/cupra_born.png";
+import cupra_formentor from "/image/carousel/cupra_formentor.png";
+import cupra_formentorVZ from "/image/carousel/cupra_formentorVZ.png";
+import cupra_leon_sportstourer from "/image/carousel/cupra_leon_sportstourer.png";
+import cupra_leon from "/image/carousel/cupra_leon.png";
+
+/** Carousel retina */
+import cupra_ateca_retina from "/image/carousel/cupra_ateca@2x.png";
+import cupra_born_retina from "/image/carousel/cupra_born@2x.png";
+import cupra_formentor_retina from "/image/carousel/cupra_formentor@2x.png";
+import cupra_formentorVZ_retina from "/image/carousel/cupra_formentorVZ@2x.png";
+import cupra_leon_sportstourer_retina from "/image/carousel/cupra_leon_sportstourer@2x.png";
+import cupra_leon_retina from "/image/carousel/cupra_leon@2x.png";
+
+/** Section one */
+import cupra_formentor_side from "/image/section/cupra_formentor_1.png";
+import cupra_formentor_front from "/image/section/cupra_formentor.png";
+
+/** Section one retina */
+import cupra_formentor_side_retina from "/image/section/cupra_formentor_1@2x.png";
+import cupra_formentor_front_retina from "/image/section/cupra_formentor@2x.png";
+
+export const images = {
+  normal: {
+    cupra_ateca,
+    cupra_born,
+    cupra_formentor,
+    cupra_formentorVZ,
+    cupra_leon_sportstourer,
+    cupra_leon,
+  },
+  retina: {
+    cupra_ateca_retina,
+    cupra_born_retina,
+    cupra_formentor_retina,
+    cupra_formentorVZ_retina,
+    cupra_leon_sportstourer_retina,
+    cupra_leon_retina,
+  },
+};
+
+export const sectionImages = {
+  normal: {
+    cupra_formentor_side,
+    cupra_formentor_front,
+  },
+  retina: {
+    cupra_formentor_side_retina,
+    cupra_formentor_front_retina,
+  },
+};
